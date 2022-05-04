cffa2bbdd328afb81f36ca3d697aa4c3410b6011
aashik <sharma81443@gmail.com>
2022-05-04 16:16:53 +0545

    banner updated

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    function initBgCover(context) {
+        if (!context) context = $('body');
+        context.find('.bg-cover').each(function () {
+            var holder = $(this);
+            var image = holder.find('> img').hide();
+            var imageSrc = image.prop('src');
+            holder.css({
+                backgroundImage: 'url(' + imageSrc + ')',
+            });
+        });
+    }
+
+    initBgCover();
+
+    /* smooth scroll*/
+    $('a.js-has-smooth[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : jQuery('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+    //banner-slider
+    $('.banner-slider').slick({
+        infinite: false,
+        dots: false,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        slidesToScroll: 1,
+    });
+
+
+});
